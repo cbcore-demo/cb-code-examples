cbe6b5611aaff26f88b85f84b4f6e0fd58d938e6
philcgithub <phil.cherry@gmail.com>
2019-10-18 09:12:59 +0100

    Initial file creation

---
+++ core/terraform/install-core-helm.tf
@@ -0,0 +1,26 @@
+resource "kubernetes_namespace" "cloudbees-core" {
+  metadata {
+    name = "cloudbees-core"
+  }
+}
+
+data "helm_repository" "cloudbees" {
+  name = "cloudbees"
+  url = "https://charts.cloudbees.com/public/cloudbees"
+}
+
+resource "helm_release" "cloudbees-core" {
+  name       = "cloudbees-core"
+  repository = data.helm_repository.cloudbees.metadata.0.name
+  chart      = "cloudbees/cloudbees-core"
+  namespace = "cloudbees-core"
+
+  set {
+    name = "name"
+    value = "cloudbees-core"
+  }
+
+  set {
+    name = "OperationsCenter.ServiceType"
+    value = "ClusterIP"
+  }
