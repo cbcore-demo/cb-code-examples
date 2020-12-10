1ac7cb534e1e8e702f3eef99c9ceacfd9a9db2d6
philcgithub <phil.cherry@gmail.com>
2020-12-10 12:16:53 +0000

    Initial upload

---
+++ core/groovy/query-connected-masters.groovy
@@ -0,0 +1,10 @@
+import com.cloudbees.opscenter.server.model.OperationsCenter
+
+OperationsCenter.instance.getConnectedMasters().each {
+    println "${it.name}"
+    println " id: ${it.id}"
+    println " idName: ${it.idName}"
+    println " grantId: ${it.grantId}"
+    println " websocket: ${it.webSocket}"
+}
+return
