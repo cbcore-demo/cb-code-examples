6d03ef1a6e37838675258b923bd04b201d8a1314
philcgithub <phil.cherry@gmail.com>
2021-03-26 13:21:19 +0000

    First version

---
+++ cd/procedures/load-propertysheet-from-file.groovy
@@ -0,0 +1,62 @@
+procedure 'Set Dynamic Options', {
+  description = ''
+  jobNameTemplate = ''
+  resourceName = ''
+  timeLimitUnits = 'minutes'
+  workspaceName = ''
+
+  step 'Set values', {
+    description = ''
+    alwaysRun = '0'
+    broadcast = '0'
+    command = '''// Example yaml File
+// - name: Option 1
+//   value: option1
+// - name: Option 2
+//   value: option2
+// - name: Option 3
+//   value: option3
+
+import com.electriccloud.client.groovy.ElectricFlow
+import org.yaml.snakeyaml.Yaml
+
+ElectricFlow ef = new ElectricFlow()
+Yaml parser = new Yaml()
+
+def propertypath = "/myProject/dynamicoptions/"
+def items = parser.load(("/tmp/dynamicoptions.yaml" as File).text)
+List results = items.name
+
+// Delete existing properties
+//def props = ef.getProperties(projectName: "$[/myProject]", propertyName: "dynamicoptions").propertySheet.property.each { prop ->
+def props = ef.getProperties(propertyName: propertypath).propertySheet.property.each { prop ->
+  println "Deleting " + propertypath + prop.propertyName
+  ef.deleteProperty(propertyName: propertypath +prop.propertyName)
+}
+
+// Load new properties  into propertySheet
+results.each {
+    def thisitem = it
+    def thisvalue = items.find { it.name == thisitem }.value
+    println "Setting property " + propertypath + thisitem + " with value " + thisvalue
+    ef.setProperty propertyName: propertypath +thisitem, value: thisvalue
+}
+
+println "New values set"'''
+    condition = ''
+    errorHandling = 'failProcedure'
+    exclusiveMode = 'none'
+    logFileName = ''
+    parallel = '0'
+    postProcessor = ''
+    precondition = ''
+    releaseMode = 'none'
+    resourceName = ''
+    shell = 'ec-groovy'
+    subprocedure = ''
+    subproject = ''
+    timeLimitUnits = 'seconds'
+    workingDirectory = ''
+    workspaceName = ''
+  }
+}
