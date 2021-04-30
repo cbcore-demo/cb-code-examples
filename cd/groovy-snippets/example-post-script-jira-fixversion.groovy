0d9ba851ed1046d16a2adb3166a54f4a814262fc
philcgithub <phil.cherry@gmail.com>
2021-04-30 11:19:24 +0100

    Changes to how URL is constructed

--- cd/groovy-snippets/example-post-script-jira-fixversion.groovy
+++ cd/groovy-snippets/example-post-script-jira-fixversion.groovy
@@ -6,8 +6,12 @@ String fileContent = file.text
 def jsonSlurper = new JsonSlurper()
 def RestResponse = jsonSlurper.parseText(fileContent)
 def JobId = ef.getProperty(propertyName: "/myJob/jobId").property.value
+def FormContentJson = ef.getProperty(propertyName: "/myJob/formContent").property.value
+def FormContent = jsonSlurper.parseText(FormContentJson)
+def BaseUrl = RestResponse.self.split("/")[0,1,2].join("/")
+def NewUrl = [BaseUrl,"projects","${FormContent.project}","versions","${RestResponse.id}"].join("/")
 ef.setProperty(propertyName: "/myStageRuntime/ec_summary/Fix Version",
-	value: """								<html><a href="${RestResponse.self}" target="_blank">${RestResponse.name}</a></html>
+	value: """								<html><a href="${NewUrl}" target="_blank">${RestResponse.name}</a></html>
 	""".stripIndent(),
 	jobId: JobId)
 ef.setProperty(propertyName: "/myPipelineRuntime/FixVersion",
