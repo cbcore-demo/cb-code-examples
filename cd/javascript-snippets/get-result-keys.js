db7ca37fca441b270f9c0a00e4d7186a1f81e712
philcgithub <phil.cherry@gmail.com>
2021-05-22 21:24:42 +0100

    First version

---
+++ cd/javascript-snippets/get-result-keys.js
@@ -0,0 +1,6 @@
+var props = ""
+var args = {
+      propertyName: "/projects/Custom022/applications/PhilTest/all_ips_18"
+}
+var propObject = api.getProperties(args).propertySheet.property;
+Object.keys(propObject);
