839240b0416605f853a5ff0065b26e965493896a
philcgithub <phil.cherry@gmail.com>
2022-03-29 16:38:31 +0100

    Improved output formatting

--- cd/dsl-snippets/browse-properties-across-environments.groovy
+++ cd/dsl-snippets/browse-properties-across-environments.groovy
@@ -1,6 +1,9 @@
 def projectNameStr = "pcherry"
 def propertyToGet = "Test"
+def result = "Environment | Property " + propertyToGet + "\n"
 getEnvironments(projectName: projectNameStr).each {
   val = getProperty(propertyName: "/projects/" + projectNameStr + "/environments/" + it.name + "/" + propertyToGet).value
-  println "Environment " + it.name + " " + propertyToGet + " = " + val
+  result += it.name + " | " + val + "\n"
 }
+
+result
